408c69d5e75365cdfc7e9657c9681ee1a9d06a33
Grtnxhor <Greatnessabolade@gmail.com>
2020-10-20 02:18:52 +0100

    js

--- js/main.js
+++ js/main.js
@@ -14,19 +14,19 @@ $(function(){
             current : ''
         },
         onStepChanging: function (event, currentIndex, newIndex) { 
-            var fullname = $('#first_name').val() + ' ' + $('#last_name').val();
-            var email = $('#email').val();
-            var phone = $('#phone').val();
-            var username = $('#username').val();
-            var gender = $('form input[type=radio]:checked').val();
-            var address = $('#address').val();
+            //var fullname = $('#first_name').val() + ' ' + $('#last_name').val();
+            //var email = $('#email').val();
+            //var phone = $('#phone').val();
+            //var username = $('#username').val();
+            //var gender = $('form input[type=radio]:checked').val();
+            //var address = $('#address').val();
 
-            $('#fullname-val').text(fullname);
-            $('#email-val').text(email);
-            $('#phone-val').text(phone);
-            $('#username-val').text(username);
-            $('#address-val').text(address);
-            $('#gender-val').text(gender);
+            //$('#fullname-val').text(fullname);
+            //$('#email-val').text(email);
+            //$('#phone-val').text(phone);
+            //$('#username-val').text(username);
+            //$('#address-val').text(address);
+            //$('#gender-val').text(gender);
 
             return true;
         }
