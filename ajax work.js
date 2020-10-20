c2b9ab86017e49fa775e7a1ed0b016ee869168e0
Grtnxhor <Greatnessabolade@gmail.com>
2020-10-20 05:23:30 +0100

    dont touch

---
+++ ajax work.js
@@ -0,0 +1,60 @@
+        var catclass     = $("#Desired_Length").val();
+        var subject      = $("#Desired_date").val();
+        var hour         = $("#Desired_Len").val();
+        var minutes      = $("#bed_num").val();
+        var catclass     = $("#moving").val();
+        var subject      = $("#secure").val();
+        var hour         = $("#pet").val();
+
+        //section 2
+
+        var minutes      = $("#first_name").val();
+        var catclass     = $("#Population_adults").val();
+        var subject      = $("#Population_children").val();
+        var hour         = $("#Population_pets").val();
+        var minutes      = $("#gender").val();
+        var catclass     = $("#smoke").val();
+        var subject      = $("#Present_Address").val();
+        var hour         = $("#time_address").val();
+        var minutes      = $("#personal_phone").val();
+        var catclass     = $("#Social").val();
+        var subject      = $("#License").val();
+        var hour         = $("#Vehicle_model").val();
+        var minutes      = $("#Vehicle_year").val();
+        var catclass     = $("#landlord_name").val();
+        var subject      = $("#landlord_phone").val();
+        var hour         = $("#rentpay").val();
+        var minutes      = $("#estate").val();
+        var minutes      = $("#estate_explain").val();
+        var catclass     = $("#evict").val();
+        var subject      = $("#evict_ex").val();
+        var hour         = $("#refused").val();
+        var minutes      = $("#refal").val();
+        var minutes      = $("#interrupt").val();
+        var catclass     = $("#interrupt_explain").val();
+        var subject      = $("#convict").val();
+        var hour         = $("#convict_ex").val();
+
+        //section 3
+
+        var minutes      = $("#income_source").val();
+        var subject      = $("#company_name").val();
+        var hour         = $("#office_address").val();
+        var minutes      = $("#position").val();
+        var minutes      = $("#Employer_Name").val();
+        var catclass     = $("#Employment_lenght").val();
+        var subject      = $("#wages").val();
+        var hour         = $("#perser").val();
+
+        //section 4
+
+        var minutes      = $("#relative_Name").val();
+        var subject      = $("#relative_Address").val();
+        var hour         = $("#relative_Phone").val();
+        var minutes      = $("#relative_relationship").val();
+        var minutes      = $("#relative_Name2").val();
+        var catclass     = $("#relative_Address2").val();
+        var subject      = $("#relative_Phone2").val();
+        var hour         = $("#relative_relationship2").val();
+        var subject      = $("#t_name").val();
+        var hour         = $("#date_filled").val();
