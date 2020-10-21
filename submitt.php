56965b644034493db20e9a11245a096b1df9f92b
Grtnxhor <Greatnessabolade@gmail.com>
2020-10-21 10:59:56 +0100

    alright

--- submitt.php
+++ submitt.php
@@ -3,7 +3,7 @@
 // $con = new mysqli("localhost","root","","online_form");
 include("functions/init.php");
 
-if(isset($_POST['renter_add']) || isset($_POST['rentee_add']) || isset($_POST['occ_len']) || isset($_POST['occ_date']) || isset($_POST['occ_length']) || isset($_POST['bed_num']) || isset($_POST['reason_move']) || isset($_POST['secure_depo']) || isset($_POST['pet_depo']) || isset($_POST['full_name']) || isset($_POST['adult']) || isset($_POST['children']) || isset($_POST['pet']) || isset($_POST['gender']) || isset($_POST['smoke']) || isset($_POST['add_now']) || isset($_POST['add_length']) || isset($_POST['phone']) || isset($_POST['social']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || isset($_POST['']) || ) {
+if(isset($_POST['renter_add']) || isset($_POST['rentee_add']) || isset($_POST['occ_len']) || isset($_POST['occ_date']) || isset($_POST['occ_length']) || isset($_POST['bed_num']) || isset($_POST['reason_move']) || isset($_POST['secure_depo']) || isset($_POST['pet_depo']) || isset($_POST['full_name']) || isset($_POST['adult']) || isset($_POST['children']) || isset($_POST['pet']) || isset($_POST['gender']) || isset($_POST['smoke']) || isset($_POST['add_now']) || isset($_POST['add_length']) || isset($_POST['phone']) || isset($_POST['social']) || isset($_POST['model']) || isset($_POST['year']) || isset($_POST['landlord_name']) || isset($_POST['landlord_phone']) || isset($_POST['rent_pay']) || isset($_POST['estate']) || isset($_POST['estate_explain']) || isset($_POST['evict']) || isset($_POST['evict_explain']) || isset($_POST['refused']) || isset($_POST['refused_ex']) || isset($_POST['interrupt']) || isset($_POST['interrupt_ex']) || isset($_POST['convict']) || isset($_POST['felony_explain']) || isset($_POST['income_source']) || isset($_POST['company']) || isset($_POST['office_add']) || isset($_POST['position']) || isset($_POST['supervisor']) || isset($_POST['supervisory']) || isset($_POST['tenant_wage']) || isset($_POST['tenant_per']) || isset($_POST['relate_name']) || isset($_POST['relate_add']) || isset($_POST['relate_phone']) || isset($_POST['relate_relation']) || isset($_POST['per_name_1']) || isset($_POST['per_add_1']) || isset($_POST['per_phone_1']) || isset($_POST['per_relation_1']) || isset($_POST['tenant_name']) || isset($_POST['app_date'])) {
 
 
 //declaration of variables
@@ -31,24 +31,24 @@ $model            = $_POST['model'];**********
 $year             = $_POST['year'];**********
 $landlord_name    = $_POST['landlord_name'];
 $landlord_phone   = $_POST['landlord_phone'];
-$cur_rent         = $_POST['cur_rent'];
+$cur_rent         = $_POST['rent_pay'];
 $estate           = $_POST['estate'];***********
-$estate_ex        = $_POST['estate_ex'];*********
+$estate_ex        = $_POST['estate_explain'];*********
 $evict            = $_POST['evict'];
-$evict_ex         = $_POST['evict_ex'];
+$evict_ex         = $_POST['evict_explain'];
 $refused          = $_POST['refused'];
 $refused_ex       = $_POST['refused_ex'];
 $interrupt        = $_POST['interrupt'];
 $interrupt_ex     = $_POST['interrupt_ex'];
-$convicted        = $_POST['convicted'];
-$convicted_ex     = $_POST['convicted_ex'];
+$convicted        = $_POST['convict'];
+$convicted_ex     = $_POST['felony_explain'];
 $income_source    = $_POST['income_source'];
 $company          = $_POST['company'];*********
 $office_add       = $_POST['office_add'];********
 $position         = $_POST['position'];
 $supervisor       = $_POST['supervisor'];
-$employ_length    = $_POST['employ_length'];
-$tenant_wage 	  = $_POST['tenant_per'];
+$employ_length    = $_POST['supervisory'];
+$tenant_wage 	  = $_POST['tenant_wage'];
 $tenant_per       = $_POST['tenant_per'];
 $relate_name      = $_POST['relate_name'];
 $relate_add       = $_POST['relate_add'];
